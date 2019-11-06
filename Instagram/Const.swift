14a205b732d4ad04a6c56d755fb209e6232cd8c9
hbsryuryu <hbsryuryu@gmail.com>
2019-11-06 19:56:56 +0900

    complete instagram app

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 濱田龍輝 on 2019/11/06.
+//  Copyright © 2019 Ryuuki.hamada. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
